e86def58a6d8797d180027fa4fc82878eec437d7
Vahid Naeini <naeini.v@gmail.com>
2024-01-30 16:02:52 -0600

    setup

---
+++ setup.sh
@@ -0,0 +1,3 @@
+curl https://raw.githubusercontent.com/iamvee/odb-to-vtk/main/multiprocess.py -o ./multiprocess.py
+curl https://raw.githubusercontent.com/iamvee/odb-to-vtk/main/odb2vtk.py -o ./odb2vtk.py
+curl https://raw.githubusercontent.com/iamvee/odb-to-vtk/main/utilities.py -o ./utilities.py
