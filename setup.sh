681385ad58793aeb5b838135c7dbdcb3ae12341a
Vahid Naeini <naeini.v@gmail.com>
2024-01-30 14:12:26 -0600

    update setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
-curl https://raw.githubusercontent.com/iamvee/odb-to-vtk/python-files/multiprocess.py -o ~/.local/bin/odb2vtk_multiprocess.py
-curl https://raw.githubusercontent.com/iamvee/odb-to-vtk/python-files/odb2vtk.py -o ~/.local/bin/odb2vtk.py
+#!/bin/bash
 
+if [ ! -d "$HOME/.local/bin" ]; then
+    mkdir -p $HOME/.local/bin
+fi
+
+if [ ! -f "$HOME/.local/bin/odb2vtk.py" ]; then
+    curl https://raw.githubusercontent.com/iamvee/odb-to-vtk/python-files/odb2vtk.py -o ~/.local/bin/odb2vtk.py
+fi
+
+if [ ! -f "$HOME/.local/bin/odb2vtk_multiprocess.py" ]; then
+    curl https://raw.githubusercontent.com/iamvee/odb-to-vtk/python-files/multiprocess.py -o ~/.local/bin/odb2vtk_multiprocess.py
+fi
+
+chmod +x ~/.local/bin/odb2vtk.py
+chmod +x ~/.local/bin/odb2vtk_multiprocess.py
 
 if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
     echo "export PATH=$HOME/.local/bin:\$PATH" >> ~/.bashrc
